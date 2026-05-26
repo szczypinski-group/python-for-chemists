0dd60489f72ad8874750b920890447d066dce483
Filip Szczypiński <filip.t.szczypinski@durham.ac.uk>
2026-05-26 17:43:18 +0100

    variables notebook

--- notebooks/02-variables.py
+++ notebooks/02-variables.py
@@ -6,6 +6,365 @@ app = marimo.App()
 
 @app.cell
 def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Variables and basic operations
+
+    Variables are essentially names for values and objects. The can have almost any name but they:
+
+    - cannot start with a number (e.g., `14all` instead of `one_for_all`)
+    - cannot contain an operator (e.g., `two-two` instead of `two_minus_two`)
+    - they are case sensitive (`DeltaG` and `deltaG` are different variables)
+    - not a "Python keyword" (e.g., `and` or `except`)
+
+    Furthermore, there are some [style recommendations](https://peps.python.org/pep-0008/):
+
+    - variables should use `snake_case` (i.e., all lowercase with underscores) rather than `CamelCase` (i.e., with words starting with uppercase); they **cannot** use `kebab-case` as minus sign is an operator!
+    - variable names should be reasonably descriptive - it is best to avoid one-one letter variables outside simple iterators (we'll cover that later).
+    - it is bad practice to redefine variables (`marimo` will not let you do that) - code gets confusing very fast!
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Assigning variables
+
+    In Python variable assignment is performed with an equal sign. The value on the right is assigned the variable name on the left:
+    """)
+    return
+
+
+@app.cell
+def _():
+    first_name = "Filip"
+    year = 2026
+    return first_name, year
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## `print()` function
+
+    Just like in mathematics, Python function take their arguments inside the brackets. So to print the value of a variable to the screen, we can simply use `print(variable_name)`.
+
+    There are are also some fancy ways to parse the arguments to the print function, most common ones being `f-strings` (where the value of the variable is substituted inside a string) and `r-strings` (where the text is parsed as _raw_, which is useful for scientific instrument outputs containing unusual characters like the backslash `\`).
+    """)
+    return
+
+
+@app.cell
+def _(first_name, year):
+    # Simple use of print
+    print(first_name)
+
+    # These two are equivalent
+    print("This year is", year)
+    print(f"This year is {year}.")
+
+    # We can also perform operations on variables
+    print(f"Next year is {year + 1}.")
+
+    # Some use of raw strings ("\n" is the new line character)
+    print("This \n will be \n three lines.")
+    print(r"This \n will be \n one line.")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Common variable types
+
+    > If it looks like a duck and quacks like a duck, it's a duck.
+
+    At the simplest level, Python is "duck typed". This means that we do not need to worry about specifying the type of a variable on assignment (we can do that, but this is a more advanced topic). The type of variable is directly inferred and the operations will be interpreted in a way that (most often) "just makes sense".
+
+    ### Integers and floating points
+
+    Those are self explanatory. They can be used for mathematical operations and often cast between one another. Try executing the following code cells.
+    """)
+    return
+
+
+@app.cell
+def _():
+    time_seconds = 90
+    time_minutes = time_seconds / 60
+
+    print(f"The time is {time_minutes} min.")
+    return
+
+
+@app.cell
+def _():
+    long_time_minutes = 42
+    long_time_seconds = long_time_minutes * 60
+
+    print(f"The other time is {long_time_seconds} s.")
+    return
+
+
+@app.cell
+def _():
+    mass = 1.6
+    light_speed = 3.0e8
+
+    print(f"Energy is {mass * light_speed ** 2}.")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Strings
+
+    The other major data type is a strong of characters. We define them using single or double quotes. Mathematical operations are slightly different for them as they result in concatenation.
+    """)
+    return
+
+
+@app.cell
+def _(first_name):
+    print(first_name * 2)
+    print(first_name + " Szczypinski")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    It can sometimes get a bit confusing if your characters are numbers - this often happens when importing data from instruments. But they can also be converted into numbers!
+    """)
+    return
+
+
+@app.cell
+def _():
+    answer = "42"
+    print(f"Multiplying a string: {answer * 2}")
+    print(f"Converting to float: {float(answer)*2}")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #### String methods and formatting
+
+    You can do a lot of cool processing to the text with Python functions. Here are some examples for now - more about [methods](https://www.w3schools.com/python/python_ref_string.asp) and [formatting](https://www.w3schools.com/python/ref_string_format.asp) can be found online.
+    """)
+    return
+
+
+@app.cell
+def _():
+    ugly_benzene = "benzene   "
+    print(f"Molecule with C6H6 formula is called '{ugly_benzene}'.")
+    return (ugly_benzene,)
+
+
+@app.cell
+def _(ugly_benzene):
+    # Lets remove trailing spaces with `rstrip()`
+    # And capitalise the first letter with `capitalize()`
+
+    print(f"Molecule with C6H6 formula is called '{ugly_benzene.rstrip().capitalize()}'.")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Collections
+
+    We often deal with collections of items, not just a single value. Common data types used for that purpse are lists, sets, and tuples. Internally, they can store any data type - and those can be mixed (e.g., you can have a list of numbers and a string "NA" for then a number is missing). You can even created lists of lists, or other objects!
+
+    The main differences are that sets are unordered and do not contain repeat elements. Lists are _mutable_ (can be modified after creation), while tuples are _immutable_ (cannot be changed).
+    """)
+    return
+
+
+@app.cell
+def _():
+    masses_set = {1.01, 4.00, 6.94, 9.01, 10.81, 12.01}
+    masses_list = [1.01, 4.00, 6.94, 9.01, 10.81, 12.01]
+    masses_tuple = (1.01, 4.00, 6.94, 9.01, 10.81, 12.01)
+    return (masses_list,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #### Accessing list elements
+
+    We can use an index (with square brackets) to extract an element of a collection. The index numbering starts with zero! A convenient shortcut is that negative indices are counted from the back of the list.
+    """)
+    return
+
+
+@app.cell
+def _(masses_list):
+    print("Hydrogen mass is", masses_list[0])
+    print("Last mass we recorded is", masses_list[-1])
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    We can also _slice_ a list to retrieve multiple elements. A convention that occurs throughout Python is that the first index is included but the second is not, with a format `[start : stop : step]`.
+
+    The default `step` is implied to be `1`. If we immit the `start` or `stop`, those will be the first and last element, respectively.
+    """)
+    return
+
+
+@app.cell
+def _(masses_list):
+    print(f"Every other mass: {masses_list[::2]}.")
+    print(f"From second to the end: {masses_list[1:]}.")
+    print(f"From second and without the last one: {masses_list[1:-1]}.")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Finally, a string is basically a collection of characters, so we can expect similar behaviour!
+    """)
+    return
+
+
+@app.cell
+def _(first_name, masses_list):
+    print(f"'{first_name}' without the first letter: '{first_name[1:]}'.")
+    print(f"'{first_name}' has {len(first_name)} characters and our table has {len(masses_list)} masses.")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Comments
+
+    Throughout this workshop, we used Markdown cells to annotate the code. This is useful for building a narrative. But somehow you want to include comments in your code that you do not want Python to read at all. Those are done with a hash symbol.
+    """)
+    return
+
+
+@app.cell
+def _():
+    some_energy = -14 # units: kJ
+
+    # This line is ignored
+    # We want to display the absolute value:
+
+    print(abs(some_energy))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Built-in functions
+
+    We have seen some built-in operations and functions in the examples above. Some other useful functions include:
+
+    | Function | Description |
+    | --- | --- |
+    | `abs()` | Absolute value |
+    | `min()` | Minimum value of a collection |
+    | `max()` | Maximum value of a collection |
+    | `float()` | Convert a value to a float |
+    | `int()` | Convert a value to an integer |
+    | `list()` | Convert an object to a list |
+    | `tuple()` | Convert an object to a tuple |
+    | `set()` | Convert an object to a set |
+    | `str()` | Convert an object to a string |
+    | `len()` | Length of an object |
+    | `round()` | Round a value |
+    | `type()` | Return object type (e.g., `float`) |
+    | `zip()` | Zips two lists together |
+
+    Feel free to create some new cells to experiment with those functions. Remember that functions take arguments (inside the brackets) and need to be somewhat meaningful (e.g., a comparison between the elements needs to exist for a minimum value to be found).
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #### Getting help
+
+    Every built-in function has documentation: `help(min)`
+    """)
+    return
+
+
+@app.cell
+def _():
+    help(max)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #### Error messages
+
+    If your input is wrong, Python will give you an error message. Those are extremely useful: they will specify which line of your code is wrong, indicate the problem with a pointer (`^`), and display a (potentially) helpful message!
+    """)
+    return
+
+
+app._unparsable_cell(
+    r"""
+    print("hello world"
+    """,
+    name="_"
+)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Functions from other modules
+
+    The power of Python lies in its modules. There are hundreds of functions available in collections that form part of the [standard library](https://docs.python.org/3/library/index.html) and even more are available to install online. The most common repository of Python packages is [PyPI](https://pypi.org/).
+
+    Once you have a package installed (either because it was formed as part of the standard library or you installed it using `pip install` in terminal), you need to import.
+
+    ### Recommended standard library modules
+
+    It is worth being aware of these and using them when appropriate. More documentation can be found online.
+
+    | Module | Description |
+    | --- | --- |
+    | `math` | Common mathematical functions |
+    | `random` | Random number generators |
+    | `pathlib` | Useful operations for file paths |
+    | `datetime` | Basic date and time types |
+    | `itertools` | Common mathematical functions |
+    | `shutil` | Higher level file operations (e.g., copying) |
+    | `pickle` | Serialisation of Python objects |
+    | `csv` | Reading and writing CSV files (e.g., from instruments) |
+    | `tomllib` | Parsing TOML files |
+    | `json` | Reading and writing JSON files |
+    """)
+    return
+
+
+@app.cell
+def _():
     return
 
 
