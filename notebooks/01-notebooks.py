91d9c74ba575ca8753aaf91671aef733d800959e
Filip Szczypiński <filip.t.szczypinski@durham.ac.uk>
2026-06-08 16:21:12 +0100

    feat: git, uv, project workflow

--- notebooks/01-notebooks.py
+++ notebooks/01-notebooks.py
@@ -54,6 +54,237 @@ def _():
     Large Language Models have changed the way we code in real life, but they do **not** eliminate the need to understand how coding and basic algorithms function – they are great to help you draft a function or troubleshoot your code, but it is crucial for you to learn how to ask the question in the best possible way. Understanding of the underlying principles or syntax rules helps you maximise the potential of new technologies. Additionally, the skill of abstracting a problem to basic components and algorithms in order to approach it is essential throughout science.
 
     I recommend that for these workshops you do not use LLMs – there are plenty of links to original resources scattered throughout the notebooks to give you some background information about what we cover.
+
+
+    ## Using `marimo` as a standalone tool
+
+    If you don't want to use the browser-based notebooks in the future but have a locally availably `marimo` notebooks, you can install them locally as a standalone tool (that is what I do).
+
+    > **Note**: this cannot be done on University-managed Windows machines yet. You can do it on Linux or on your personal computer.
+
+    For installing anything Python-related, I **very strongly recommend** the `uv` package (unfortunately, you cannot get it from AppsAnywhere yet). Installation instructions for all platforms are [here](https://docs.astral.sh/uv/getting-started/installation/). Once you have `uv` installed, you can use marimo extremely easily - check the [website](https://docs.astral.sh/uv/guides/integration/marimo/) for instructions but they basically boil down to typing:
+
+    ```sh
+    uvx marimo edit
+    ```
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Git and version control
+
+    Modern coding projects use [version control](https://en.wikipedia.org/wiki/Version_control) for controlling and tracking different version of the source code. Probably the most widespread version control system is [Git](https://en.wikipedia.org/wiki/Git) - it was developed by Linus Torvalds for the development of the Linux kernel
+
+    ## GitHub
+
+    GitHub is one of the most populars developer platforms for storing, managing, and sharing code. It uses Git and provides a number of other useful features. Since 2018 it has been owned by Microsoft, so I feel free to [explore alternatives](https://european-alternatives.eu/category/version-control-services) if you disagree with Microsoft practices.
+
+    My recommendation for my research group is that **all **code development should be done with version control on GitHub - you might have noticed that these workbooks are also a GitHub repository! Best practice is to work on the non-`main` branch for any actual development and then create a "pull request" for the tested code to be included in the main branch. For more information about how to use GitHub, see: https://docs.github.com/en/get-started/start-your-journey/hello-world/.
+
+    > **Recommendation**: If you do not have a GitHub account, it is a good idea to create one now. Go to https://github.com/, sign up, create a new (private if you wish) repository called "python-introduction", and upload this notebook there. You can use it as a base for all Python in these workbooks and then create new repositories if needed, e.g., for lab write-ups or your MChem project!
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Python projects: complete workflow
+
+    This is a **_much_ more advanced topic**. You might want to skip it as you will not be able to currently do this on a University-managed Windows computer. Howver, I am including it for your future reference as I believe it is very important and useful. You should be able to do it on a Linux machine or your personal computer.
+
+    For implementing different versions of Python and packages, use the `pip` interface from the `uv` package. Installation instructions for all platforms are [here](https://docs.astral.sh/uv/getting-started/installation/). Most of your code should probably be what `uv` considers a [library](https://docs.astral.sh/uv/concepts/projects/init/#libraries), i.e. isolated code with `src` layour, `py.typed` indicator, and metadata stored in a `pyproject.toml` file.
+
+    ```sh
+    uv init --lib YOUR-PROJECT
+    ```
+
+    And that is basically it. For any dependency (e.g. `matplotlib`) just use:
+
+    ```sh
+    uv add matplotlib
+    ```
+
+    If you want the dependency to only be added to a specific group (e.g., developer dependencies are only installed if you get you software as `pip install package[dev]`):
+
+    ```sh
+    uv add --dev pre-commit
+    ```
+
+    > **Note**: have a look at our [pyMultiwfn repository](https://github.com/szczypinski-group/pyMultiwfn) for a real-life example. In particular, have a look at the contents of the `.pre-commit-config.yaml` (which defines the settings of `pre-commit`) and the `pyproject.toml` file (all other metadata, including Python versions, linting settings, etc.). Don't worry about `uv.lock` - this is added automatically by `uv`. I highly recommend having a `README.md` (which is then rendered on the GitHub repository main page) and `LICENSE` (for copyright protection).
+
+    ## Depositing with GitHub
+
+    Once you have created your code, it is important to add in to GitHub. Create a **new repository** (top right), give it a name, and set its visibility (probably **Private** to begin with). **Do not initialise** with any files (README, etc.) as you will already have them on your local computer.
+
+    Then, on your local machine where you crated the code (see section above):
+
+    ```sh
+    # Navigate to the folder
+    cd PATH/TO/YOUR-PROJECT
+    # Initialise the repository
+    git init
+    # Set up the main branch
+    git branch -M main
+    ```
+
+    You might want to add files to `.gitignore` so that theyy are not versioned by git, you can do it by editing the file or from the command line e.g.:
+
+    ```sh
+    echo "__pycache__/" > .gitignore
+    ```
+
+    And when you are ready, make the first commit:
+
+    ```sh
+    # Stage all modified files
+    git add .
+    # Commit with a message
+    git commit -m "Initial commit"
+    ```
+
+    You will also need to add the GitHub repository as `remote`. Copy the address from GitHub (big `<> Code` button) and then:
+
+    ```sh
+    git remote add origin git@github.com:USERNAME/your-repo-name.git
+    git push -u origin main
+    ```
+
+    ## Best practices
+
+    > Commit often in small hunks - **code dumps are almost impossible to review!**
+    {.is-warning}
+
+    1. We try to follow appropriate style guides (e.g., [Python PEP8](https://peps.python.org/pep-0008/), [Markdown](https://google.github.io/styleguide/docguide/style.html), [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/)).
+    2. Install a linter for your IDE, I recommend [ruff](https://docs.astral.sh/ruff/installation/) with some good default settings (see example `pyproject.toml`).
+    3. Structure your code as a Python project and install [pre-commit](https://pre-commit.com/) to ensure consistency. **Use [uv](https://docs.astral.sh/uv/) for package and environment managing.**
+    4. Use [type-checking](https://docs.python.org/3/library/typing.html) in your Python code (by including `py.typed` file), make sure that you write docstrings (`interrogate`) and tests (`pytest`) as you go. Comment often to help others understand your intentions.
+    5. Familiarise yourself with command line editors such as `vim` or `nano` for quick edits on the supercomputing cluster.
+    6. Follow [git best practices](https://docs.github.com/en/get-started/using-git). **Commit often**, branch/fork actively! If your commit is getting too large, **use patch mode**: `git add --patch --interactive`.
+    7. Use the [Python standard library](https://docs.python.org/3/library/index.html) whenever possible (e.g., `time`, `pathlib`) instead of external codes to reduce complexity and dependencies.
+
+    ## Developing code
+
+    As mentioned above, make sure you have `ruff` installed:
+
+    ```sh
+    uv tool install ruff@latest
+    uv add --dev ruff
+    ```
+
+    and same for `pre-commit`:
+
+    ```sh
+    uv add --dev pre-commit
+    uv run pre-commit install
+    ```
+
+    Now `git` will stop you from committing code that does not conform to the standards set up in the package specification.
+
+    ### Example `pyproject.toml`
+
+    > **Note**: have a look at our [pyMultiwfn repository](https://github.com/szczypinski-group/pyMultiwfn) for a real-life example.
+
+    On top of the `pyproject.toml` generated by `uv`, you can add some useful (potentially) settings for programming tools:
+
+    ```toml
+    [tool.interrogate]
+    verbose                    = 1
+    ignore-setters             = true
+    ignore-property-decorators = true
+    ignore-magic               = true
+    ignore-private             = true
+    ignore-semiprivate         = true
+    exclude                     = ["tests/**", "menu.py"]
+
+    [tool.ruff]
+    line-length = 79
+    target-version = "py311"
+    lint.select = [
+        "E",   # pycodestyle
+        "F",   # Pyflakes
+        "UP",  # pyupgrade
+        "B",   # flake8-bugbear
+        "SIM", # flake8-simplify
+        "TD",  # flake8-todos
+        "PTH", # flake8-use-pathlib
+        "ANN", # flake8-annotations
+        "I",   # isort
+        "N",   # PEP8 naming
+        "D",   # pydocstyle
+    ]
+    lint.ignore = [
+        "D102",  # Missing docstring in public method (interrogate)
+        "D103",  # Missing docstring in public function (interrogate)
+        "D105",
+        "TD003",  # TODO GH issue links are not always possible
+    ]
+
+        [tool.ruff.lint.pydocstyle]
+        convention = "numpy"
+
+    [tool.mypy]
+    exclude = "tests/"
+
+    [tool.pytest.ini_options]
+    addopts = "--cov --cov-report=term-missing"
+
+    [tool.coverage.run]
+    source = ["pymultiwfn"]
+    omit = [
+        "tests/*",
+        "**/__init__.py",
+        "setup.py",
+    ]
+
+    [tool.coverage.report]
+    skip_covered = true
+    ```
+
+    ### Example `.pre-commit-config.yaml`
+
+    To ensure that code is uniform and "up to standard", we can use tools like pre-commit. This stops the developer from sending inconsistent code to GitHub by introducing a number of tests that must pass. Example configuration could look like this:
+
+    ```yaml
+
+    repos:
+      - repo: https://github.com/astral-sh/uv-pre-commit
+        # uv version.
+        rev: 0.11.8
+        hooks:
+          - id: uv-lock
+
+      - repo: https://github.com/astral-sh/ruff-pre-commit
+        # Ruff version.
+        rev: v0.15.12
+        hooks:
+        # Run the linter.
+        - id: ruff-check
+          args: [ --fix ]
+        # Run the formatter.
+        - id: ruff-format
+
+      - repo: https://github.com/econchick/interrogate
+        rev: 1.7.0
+        hooks:
+          - id: interrogate
+            args: [--config=pyproject.toml]
+            pass_filenames: false
+
+      - repo: https://github.com/pre-commit/mirrors-mypy
+        rev: v1.20.2
+        hooks:
+          - id: mypy
+            pass_filenames: false
+            args: ["--config-file=pyproject.toml", "src"]
+            additional_dependencies: [
+                "types-requests",
+                "pandas-stubs"
+            ]
+    ```
     """)
     return
 
