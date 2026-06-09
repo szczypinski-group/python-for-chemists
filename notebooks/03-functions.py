ed11747caeb377b9dad2e7727f8d52fe236bf6fc
Filip Szczypiński <filip.t.szczypinski@durham.ac.uk>
2026-06-09 16:04:33 +0100

    docs: restructure functions

--- notebooks/03-functions.py
+++ notebooks/03-functions.py
@@ -7,7 +7,7 @@
 
 import marimo
 
-__generated_with = "0.23.8"
+__generated_with = "0.23.9"
 app = marimo.App()
 
 with app.setup:
@@ -30,24 +30,102 @@ def _():
 
     where the `arguments` are essentially variables used **inside** the function and `return` keyword specifies what the function should give back to the user (typically so that it can be assigned to a variable or printer). The `return` part is optional (e.g., if you just want the function to print).
 
-    Let's write a function that will convert Celsius to Kelvin and see how we can then expand on its functionality.
+    Once we have _defined_ a function (with `def`) we _call_ it, by typing its name and any arguments in the parentheses. Only once we've called it, the code inside the function will be executed!
+
+    ```python
+    function_name()
+    ```
+
+    > **Note**: if a function definition is on its own in a Python cell in `marimo`, it can be imported into your other notebooks. It's [_reusable_](https://docs.marimo.io/guides/reusing_functions/). This is very powerful - you can keep a "main notebook" with your useful helper functions for data analysis and import them when needed!
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Indentation
+
+    In some programming languages, blocks of code are enclosed in curly brackets or isolated some other way. In Python, we use whitespaces (tabs and spaces) for that - there are no strict rules, but you have to be consistent. I recommend a single tab (converted to four spaces) as the indentation pattern - this is the default in most notebooks and editors.
+
+    ```python
+    # This is the default code level
+
+    import math
+
+    # Variables defined at this code level
+    # are available everywhere deeper
+    # so can be considered "constants"
+    # Python convention is to name them in ALL-CAPS
+
+    LIGHT_SPEED = 3.0e8
+
+    def get_energy(mass):
+        # Everything at this indent level is a block
+
+        # Variables defined here are only available
+        # within this block (or deeper in)
+        # we can also use function arguments (`mass`)
+        # within thie block (i.e., function definition).
+
+        print("We are inside now")
+
+        # `energy` is only inside the function
+        # uses function argument and a constant
+
+        energy = mass * LIGHT_SPEED ** 2
+
+        return energy
+
+    # Back to the default level
+    # Rest of the code ...
+
+    ```
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Let's see that in practice in a Python cell (I'm using the advanced `try`...`except` block so that the code can execute - it would throw an error otherwise, try for yourself):
     """)
     return
 
 
 @app.cell
+def _(mass):
+    import math
+
+    LIGHT_SPEED = 3.0e8
+
+    def get_energy(mass):
+        print(f"LIGHT_SPEED inside the function: {LIGHT_SPEED}")
+        return mass * LIGHT_SPEED ** 2
+
+    _electron = 9.10e-31
+    _electron_energy = get_energy(_electron)
+
+    try:
+        print(mass)
+    except Exception as e:
+        print(f"Outside the function: {e}.")
+
+    print(f"Energy of an electron: {_electron_energy} J.")
+    return
+
+
+@app.cell(hide_code=True)
 def _():
-    # Here `temp` it a function parameter that has to be specified
-    # and then it exists to be used only within the function
-    # - it's different from any other temp in the Python script!
+    mo.md(r"""
+    Let's write a simple function that will convert Celsius to Kelvin and see how we can then expand on its functionality.
+    """)
+    return
 
-    temp = 0
 
-    def from_celsius0(temp):
-        # "function scope" temp is different from "global scope" temp (=0)
-        print(f"Temperature in Kelvin is {temp+273.15} K.")
-
-    return (from_celsius0,)
+@app.function
+def print_kelvin(temp):
+    print(f"Temperature in Kelvin is {temp+273.15} K.")
 
 
 @app.cell(hide_code=True)
@@ -59,16 +137,16 @@ def _():
 
 
 @app.cell
-def _(from_celsius0):
+def _():
     body_temp = 36.6
 
     # We can call the function just like before.
-    from_celsius0(temp=45)
-    from_celsius0(temp=body_temp)
+    print_kelvin(temp=45)
+    print_kelvin(temp=body_temp)
 
     # We can omit the argument names.
-    from_celsius0(20)
-    from_celsius0(body_temp)
+    print_kelvin(20)
+    print_kelvin(body_temp)
     return (body_temp,)
 
 
@@ -82,11 +160,14 @@ def _():
 
 app._unparsable_cell(
     r"""
+    # Here we define the function:
     def hi():
         # COMPLETE HERE
 
-    # Let's see if it works. Running this cell should display "Hello!"
+    # Let's see if it works, here we call it:
     hi()
+
+    # Running this cell should display "Hello!"
     """,
     name="_"
 )
@@ -97,7 +178,7 @@ def _():
     mo.md(r"""
     ## `return` statements
 
-    Most often we do not want the function to just print some result to the screen - we want to use it. This is when the `return` statements come in handy. Imagine wanting to calculate the $\Delta G_\textrm{rxn}$ for a reaction, knowing the equilibrium constant $K_\textrm{eq}$ and the temperature in $\degree C$.
+    Most often we do not want the function to just print some result to the screen - we want save the result of calculation as a variable to use it. This is when the `return` statements come in handy. Imagine wanting to calculate the $\Delta G_\textrm{rxn}$ for a reaction, knowing the equilibrium constant $K_\textrm{eq}$ and the temperature in $\degree C$.
 
     Complete the following cell and execute it to see if it worked.
     """)
@@ -106,18 +187,16 @@ def _():
 
 app._unparsable_cell(
     r"""
-    import math
-
-    def from_celsius1(temp):
+    def get_kelvin(temp):
         # you can define an auxiliary variable (e.g. "kelvin_temp")
         # or redefine the local variable with temp += ...
         # or return the result of an operation straight away
-    
+
         # FIXME: include a return statement
 
 
     def gibbs(k_eq, temp):
-        dG = - 8.314472 * from_celsius1(temp) * math.log(k_eq)
+        dG = - 8.314472 * get_kelvin(temp) * math.log(k_eq)
         return dG
     """,
     name="_"
@@ -125,11 +204,11 @@ app._unparsable_cell(
 
 
 @app.cell(hide_code=True)
-def _(body_temp, from_celsius1, gibbs):
+def _(body_temp, get_kelvin, gibbs):
     def function_passed():
         passed = False
         try:
-            test_c = from_celsius1(body_temp) == 309.75
+            test_c = get_kelvin(body_temp) == 309.75
             test_g = abs(gibbs(10, 18) - (-5574.00)) < 1.0
 
             passed = test_c and test_g
@@ -157,7 +236,7 @@ def _():
     ```python
     temp = 35
 
-    from_celsius(temp=temp)
+    get_kelvin(temp=temp)
     ```
 
     Arguments that do not have any value assigned to them when defining a function are called _positional arguments_. They are compulsory to be passed when calling the funciton.
@@ -170,7 +249,7 @@ def _():
 
 
 @app.function
-def from_celsius2(
+def from_celsius_usa(
     temp,
     fahrenheit = False,
 ):
@@ -184,8 +263,8 @@ def from_celsius2(
 
 @app.cell
 def _(body_temp):
-    print(f"Body temperature in Kelvin is: {from_celsius2(body_temp)} K.")
-    print(f"Body temperature in Fahrenheit is: {from_celsius2(body_temp, fahrenheit=True):.2f} F.")
+    print(f"Body temperature in Kelvin is: {from_celsius_usa(body_temp)} K.")
+    print(f"Body temperature in Fahrenheit is: {from_celsius_usa(body_temp, fahrenheit=True):.2f} F.")
     return
 
 
@@ -200,16 +279,23 @@ def _():
 @app.cell
 def _(body_temp):
     # temp first, fahrenheit second
-    print(f"Body temperature in Fahrenheit is: {from_celsius2(temp=body_temp, fahrenheit=True):.2f} F.")
+    print(f"Body temperature in Fahrenheit is: {from_celsius_usa(temp=body_temp, fahrenheit=True):.2f} F.")
 
     # fahrenheit first, temp second
-    print(f"Body temperature in Fahrenheit is: {from_celsius2(fahrenheit=True, temp=body_temp):.2f} F.")
+    print(f"Body temperature in Fahrenheit is: {from_celsius_usa(fahrenheit=True, temp=body_temp):.2f} F.")
 
-    # split for shorter code lines
+    # Split for shorter code lines
     print(
         f"Body temperature in Fahrenheit is: "
-        f"{from_celsius2(temp=body_temp, fahrenheit=True):.2f} F."
+        f"{from_celsius_usa(temp=body_temp, fahrenheit=True):.2f} F."
     )
+
+    # Split function call even further
+    _temp_f = from_celsius_usa(
+        temp=body_temp,
+        fahrenheit=True
+    )
+    print( f"Body temperature in Fahrenheit is: {_temp_f:.2f} F.")
     return
 
 
@@ -228,7 +314,7 @@ def _():
 
 
 @app.function
-def from_celsius3(
+def from_celsius(
     temp,
     fahrenheit = False,
 ):
@@ -259,7 +345,7 @@ def from_celsius3(
 @app.cell(hide_code=True)
 def _():
     mo.md(r"""
-    Now try typing `from_celsius3()` in the next cell and see how you get a little helpful paragraph about the function!
+    Now try typing `from_celsius()` in the next cell and see how you get a little helpful paragraph about the function!
     """)
     return
 
@@ -267,6 +353,14 @@ def _():
 @app.cell
 def _(from_):
     from_
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    # Advanced topics
+    """)
     return
 
 
@@ -321,12 +415,12 @@ def print_temp(
     temp : float | int,
 ) -> None:
     """Print value of the temperature.
-    
+
     Arguments
     ---------
     temp
         Temperature in Celsius.
-        
+
     """
     print(f"Temperature is {temp} degrees.")
 
@@ -336,6 +430,145 @@ def _():
     mo.md(r"""
     While it might feel completely unnecessary, starting your function definition with docstrings and explicit types helps you structure your thoughts and makes coding much easier down the line.
     """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Classes
+
+    This is a **more advanced topic** that you might want to skip on your first exposure to Python.
+
+    You can see above that we defined two separate variables for each point and calculated the difference between them with another set of variables. What if we had 1,000 different points? We can create a [`class`](https://docs.python.org/3/tutorial/classes.html) that contains all the information about the points (_attributes_) and some useful functions (_methods_) related to them. We will learn more about functions in the next section, but let's consider this example:
+    """)
+    return
+
+
+@app.class_definition
+class PointExample:
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        z: float,
+    ):
+        self.x = x 
+        self.y = y
+        self.z = z
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    > **Note**: so called "magic methods" in Python start with two underscores (e.g., `__init__`). They preform some special functions related to the inner working of the programming language; in this case, `__init__` initialises an instance of the class.
+
+    Where `self` is the conventional name for the first parameter, which corresponds to the instance of the class _itself_. We can can create points consistently as _instances_ of class `Point` and access them usign the familiar "dot" notation:
+    """)
+    return
+
+
+@app.cell
+def _():
+    _point1 = PointExample(1, 1, 1)
+    _point2 = PointExample(3, 4, 5)
+
+    print(f"Point(x={_point2.x}, y={_point2.y}, z={_point2.z})")
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    but the main strength comes from then isolating relevant methods together. Try writing a method that returns the distance between two points for this new complete class representing a `Point`.
+
+    > **Note**: as we will be defining arguments for methods inside the `Point` class, the `Point` does not yet exist - we can pass it as a string in the type hints (see `distance_from()` below).
+    """)
+    return
+
+
+@app.cell
+def _(diff_x, diff_y, diff_z, sqrt):
+    class Point:
+        """Represents a point in 3D space.
+
+        Attributes
+        ----------
+        x, y, z : float
+            Coordinates in the 3D space.
+
+        Methods
+        -------
+        distance_from(point)
+            Returns Euclidean distance from another point.
+
+        """
+        def __init__(
+            self,
+            x: float,
+            y: float,
+            z: float,
+        ):
+            self.x = x 
+            self.y = y
+            self.z = z
+
+        def __str__(
+            self
+        ):
+            return f"Point(x={self.x}, y={self.y}, z={self.z})"
+
+        def distance_from(
+            self,
+            point : "Point"
+        ):
+            # FIXME: Define local variables for diff_x/y/z
+
+            return sqrt(diff_x ** 2 + diff_y ** 2 + diff_z ** 2)
+
+    _point1 = Point(x=1, y=1, z=1)
+    _point2 = Point(3, 4, 5)
+
+    _point1.distance_from(_point2)
+    return (Point,)
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    It is often useful to define some more magic methods, such as `__str__` or `__repr__` – those are used when you try to print the object as a string. Compare the output of those two commands:
+    """)
+    return
+
+
+@app.cell
+def _(Point):
+    import numpy as np
+
+    def check_point():
+        _point1 = Point(1, 1, 1)
+        _point2 = Point(3, 4, 5)
+
+        try:
+            passed = np.isclose(_point1.distance_from(_point2), 5.39, atol=0.01)
+
+            if passed:
+                return mo.callout("✅ Correct", kind="success")
+            else:
+                return mo.callout("❌ Not quite.", kind="danger")  
+
+        except Exception as e:
+            passed = False
+            return mo.callout(f"❌ Python error: {e}.", kind="danger")
+
+    check_point()
+    return
+
+
+@app.cell
+def _(Point):
+    print(f"Class for which we defined __str__: {Point(1,1,1)}")
+    print(f"Class for which we didn't: {PointExample(1,1,1)}")
     return
 
 
@@ -423,11 +656,6 @@ app._unparsable_cell(
     """,
     name="_"
 )
-
-
-@app.cell
-def _():
-    return
 
 
 if __name__ == "__main__":
